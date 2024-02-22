5e1a0f152223c4fe7a294b8cd149109ef489e1ab
fayeyoussou <fayeyoussoupha@gmail.com>
2024-02-22 09:02:17 +0000

    Update settings.py

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -13,13 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -36,7 +33,6 @@ ALLOWED_HOSTS = [
     '.vercel.app'
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,8 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-CSRF_TRUSTED_ORIGINS = ['https://*.https://inventorymanagement-production-619e.up.railway.app','https://*.127.0.0.1']
-
+CSRF_TRUSTED_ORIGINS = ['*', 'https://*.railway.app', 'https://*.127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -91,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'inventory_management.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -101,7 +95,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -121,7 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -132,7 +124,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
